b667e3d84edb29e1bdc757640ce829751c2c79d0
monisha <monisha@localhost>
2023-03-13 13:52:17 +0530

    First Commit

---
+++ src/main/java/Monisha/Monishamvc/App.java
@@ -0,0 +1,13 @@
+package Monisha.Monishamvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Spring!" );
+    }
+}
